0019640bb758f7808172afbac6ff2a9116913a9e
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-03-20 13:07:02 -0500

    added voice verification to service for use when the user does not have SMS capability

--- phone_verification_service.rb
+++ phone_verification_service.rb
@@ -1,21 +1,29 @@
 # Originally developed as part of a Rails app
-# http://blog.bigbinary.com/2015/01/12/phone-verification-using-twilio.html
+# http://blog.bigbinary.com/2015/03/18/voice-based-phone-verification-using-twilio.html
 
 class PhoneVerificationService
-  attr_reader :user
+  attr_reader :user, :verification_through
 
-  def initialize(options)
-    @user = User.find(options[:user_id])
+  VERIFICATION_THROUGH_SMS  = :sms
+  VERIFICATION_THROUGH_CALL = :call
+
+  def initialize options
+    @user                 = User.find(options[:user_id])
+    @verification_through = options[:verification_through] || VERIFICATION_THROUGH_SMS
   end
 
   def process
-    send_sms
+    if verification_through == VERIFICATION_THROUGH_SMS
+      perform_sms_verification
+    else
+      make_call
+    end
   end
 
   private
 
   def from
-    # Add your twilio phone number (programmable phone number)
+    #phone number given by twilio
     Settings.twilio_number_for_app
   end
 
@@ -25,14 +33,7 @@ class PhoneVerificationService
   end
 
   def body
-    "Please reply with this code '#{user.phone_verification_code}' to
-    verify your phone number"
-  end
-
-  def twilio_client
-    # Pass your twilio account sid and auth token
-    @twilio ||= Twilio::REST::Client.new(Settings.twilio_account_sid,
-                                         Settings.twilio_auth_token)
+    "Please reply with this code '#{user.phone_verification_code}' to verify your phone number"
   end
 
   def send_sms
@@ -45,5 +46,34 @@ class PhoneVerificationService
     )
   end
 
-end # class PhoneVerificationService
+  def make_call
+    Rails.logger.info "Call: From: #{from} To: #{to}"
 
+    twilio_client.account.calls.create(
+      from: from,
+      to: to,
+      url: callback_url
+    )
+  end
+
+  def perform_sms_verification
+    begin
+      send_sms
+    rescue Twilio::REST::RequestError => e
+      return make_call if e.message.include?('is not a mobile number')
+      raise e.message
+    end
+  end
+
+  def callback_url
+    Rails.application.routes.url_helpers
+      .phone_verifications_voice_url(host: Settings.app_host,
+                                     verification_code: user.phone_verification_code)
+  end
+
+  def twilio_client
+    @twilio ||= Twilio::REST::Client.new(Settings.twilio_account_sid,
+                                         Settings.twilio_auth_token)
+  end
+
+end # class PhoneVerificationService

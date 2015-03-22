5558294d767a9b168f98d6570caf970e3bf9eb21
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-03-22 17:35:20 -0500

    added ability to turn off the tire kicking; removed the hands_on method for now because of a version conflict over slop between pry and cli_helper

--- kick_the_tires.rb
+++ kick_the_tires.rb
@@ -7,9 +7,10 @@
 
 require 'awesome_print'
 require 'pathname'
-require 'pry'
 
 module KickTheTires
+
+  @ktt_disabled = false
 
   if caller.empty?
     required_by_filename = __FILE__
@@ -20,6 +21,7 @@ module KickTheTires
   SOURCE = Pathname.new(required_by_filename).readlines.map{|a_line| a_line.chomp.strip}
 
   def show_source
+    return if out_for_a_drive?
     puts
     puts "-"*75
     a_string    = caller.last
@@ -28,12 +30,14 @@ module KickTheTires
   end
 
   def show(a_thing)
+    return if out_for_a_drive?
     show_source
     puts "Showing #{a_thing.class} #=>"
     ap a_thing, {indent: 2, raw: true}
   end
 
   def assert(its_true)
+    return if out_for_a_drive?
     unless its_true
       show_source
       print 'Result #=> '
@@ -42,6 +46,7 @@ module KickTheTires
   end
 
   def refute(its_really_true)
+    return if out_for_a_drive?
     if its_really_true
       show_source
       print 'Result #=> '
@@ -50,6 +55,7 @@ module KickTheTires
   end
 
   def assert_equal(expected_this, got_that)
+    return if out_for_a_drive?
     unless expected_this.to_s == got_that.to_s
       show_source
       puts 'Result #=>'
@@ -59,12 +65,30 @@ module KickTheTires
     end
   end
 
+  # disable the asserts and shows
+  def take_it_for_a_spin
+    @ktt_disabled = true
+  end
+
+  def give_the_keys_back
+    @ktt_disabled = false
+  end
+
+  def out_for_a_drive?
+    @ktt_disabled
+  end
+
+
   # TODO: Need a little more thinking about this
+  #       pry currently uses and older version of pry
+  #       than cli_helper
+=begin
   def hands_on
     show_source
     puts "Entering pry.  Type 'help' for available commands."
     puts "Enter the command 'up' to kick the tires on the new ride."
     binding.pry
   end
+=end
 
 end # module KickTheTires

6329aef1ef5efd5e22d3ced5014f2036e2906057
Dewayne VanHoozer <dvanhoozer@42six.com>
2016-05-24 15:12:58 -0500

    A way of adding command line parsing to rake tasks

---
+++ rake_task_arguments.rb
@@ -0,0 +1,21 @@
+# rake_task_arguments.rb
+
+require 'docopt'
+
+class RakeTaskArguments
+  def self.parse(task_name, argument_spec, args)
+    arguments = {}
+
+    begin
+      args.delete_at(1) if args.length >= 2 and args.second == '--'
+
+      Docopt::docopt(argument_spec, {:argv => args}).each do |key, value|
+        arguments[key == "--" ? 'task_name' : key.gsub('--', '')] = value
+      end
+    rescue Docopt::Exit => e
+      abort(e.message)
+    end
+
+    return arguments
+  end # def self.parse_arguments(task_name, argument_spec, args)
+end # class RakeTaskArguments

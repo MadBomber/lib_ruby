869604e342da4de56209f23b6d3e5ffd2d208036
Dewayne VanHoozer <dewayne@mars.local>
2018-03-30 21:57:39 -0500

    added a time_this method that uses a monotonic clock from Process class

---
+++ time_this.rb
@@ -0,0 +1,12 @@
+#######################################################
+###
+##  File: time_this.rb
+##  Desc: Correct way to measure time to avoid NTP interference
+##  See:  https://blog.dnsimple.com/2018/03/elapsed-time-with-ruby-the-right-way/
+#
+
+def time_this &block
+  _time_this_starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  block.call
+  Process.clock_gettime(Process::CLOCK_MONOTONIC) - _time_this_starting
+end

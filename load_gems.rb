1ad3df64a3283eb29b6737d2689d46e547d1ea65
Dewayne VanHoozer <dvanhoozer@gmail.com>
2018-08-16 18:03:59 -0500

    A little code that ensures that a small app's gems are loaded and required

---
+++ load_gems.rb
@@ -0,0 +1,40 @@
+# load_gems.rb
+# Uses bundle/inline to insure that gem libraries are available
+# Its intended for small apps.
+# Note that the bundler/inline sets the load path so that only
+# the specified gems are available.
+
+begin
+  require 'bundler/inline'
+rescue
+  system "gem install bundler"
+  require 'bundler/inline'
+end
+
+# TODO: Might want to allow specific versions of the gems in addition to their names
+
+def load_gems(an_array_of_strings)
+
+  print "Installing gems as necessary ... " if $debug
+  gemfile do
+    source 'https://rubygems.org'
+    an_array_of_strings.each do |gem_name|
+      print gem_name + ' ' if $debug
+      gem gem_name
+    end
+  end
+
+  puts 'done' if $debug
+
+end # def load_gems(an_array_of_strings)
+
+__END__
+
+Typical Usage:
+
+require 'load_gems'
+load_gems %w[
+  awesome_print cli_helper debug_me loofah mail progress_bar rethinkdb_helper
+]
+
+

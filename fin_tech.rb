60464b17576e2e20e21264da6f9f6bb935c09f75
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-07-31 11:53:01 -0500

    added a few more methods

--- fin_tech.rb
+++ fin_tech.rb
@@ -8,7 +8,7 @@ class FinTech
   class << self
 
     #######################################################################
-    # Moving Averages
+    # Moving Averages - Simple Moving Average (SMA)
     #
     # This method takes in an array of historical prices for a stock and a 
     # period (the number of days to calculate the moving average over). It 
@@ -30,9 +30,12 @@ class FinTech
       end
       return moving_averages
     end
+    alias_method :sma, :moving_averages
 
 
-    def trend(data, period)
+    # Simple Moving Average Trend - up or down with angle
+    #
+    def sma_trend(data, period)
       closes    = data.map{|r| r['Adj Close'].to_f}
       sma       = moving_averages(closes, period)
       last_sma  = sma.last
@@ -214,6 +217,126 @@ class FinTech
       
       return [macd_line, signal_line]
     end
+
+
+    # Calculates the Donchian Channel for a given period and input data.
+    #
+    # In the domain of computer programming, a Donchian Channel is a technical 
+    # analysis indicator used to identify potential breakouts and trend reversals 
+    # in financial markets. It consists of three lines: the upper channel line, 
+    # the lower channel line, and the middle line.
+    #
+    # The upper channel line is calculated by finding the highest high over 
+    # a specified period of time, while the lower channel line is calculated 
+    # by finding the lowest low over the same period. The middle line is simply 
+    # the average of the upper and lower channel lines.
+    #
+    # @param period [Integer] The period for the Donchian Channel.
+    # @param input_data [Array] An array of values.
+    # @return [Array] An array of arrays representing the Donchian Channel.
+    #
+    def donchian_channel(period, input_data)
+      max = -999999999
+      min = 999999999
+      donchian_channel = []
+
+      input_data.each_with_index do |value, index|
+        value = value.to_f
+        max = value if value > max
+        min = value if value < min
+
+        if index >= period - 1
+          donchian_channel << [max, min, (max + min) / 2]
+          max = -999999999
+          min = 999999999
+        end
+      end
+
+      donchian_channel
+    end
+
+
+    # Calculates the True Range (TR) for a given set of price data.
+    #
+    # The True Range is a measure of the price volatility of a security over a given
+    # period.  It is calculated as the greatest of the following three values:
+    # - The difference between the current high and the current low.
+    # - The absolute value of the difference between the current high and the previous 
+    #   close.
+    # - The absolute value of the difference between the current low and the previous 
+    #   close.
+    #
+    # The True Range helps to capture the true extent of price movement, taking 
+    # into account potential gaps or price jumps between periods. It is often used 
+    # as a component in calculating other indicators, such as the Average True Range.
+    #
+    # @param high_prices [Array] An array of high prices.
+    # @param low_prices [Array] An array of low prices.
+    # @param previous_closes [Array] An array of previous closing prices.
+    # @return [Array] An array of True Range values.
+    #
+    def true_range(high_prices, low_prices, previous_closes)
+      true_ranges = []
+
+      high_prices.each_with_index do |high, index|
+        low = low_prices[index]
+        previous_close = previous_closes[index]
+
+        true_range = [
+          high - low,
+          (high - previous_close).abs,
+          (low - previous_close).abs
+        ].max
+
+        true_ranges << true_range
+      end
+
+      true_ranges
+    end
+    alias_method :tr, :true_range
+
+
+    # Calculates the Average True Range (ATR) for a given set of price data.
+    #
+    # The Average True Range is an indicator that calculates the average of the 
+    # True Range values over a specified period. It provides a measure of the 
+    # average volatility of a security over that period.
+    #
+    # The ATR is commonly used to assess the volatility of a security, identify 
+    # potential trend reversals, and determine appropriate stop-loss levels. Higher 
+    # ATR values indicate higher volatility, while lower ATR values indicate lower 
+    # volatility.
+    #
+    # For example, a 14-day Average True Range would calculate the average of the 
+    # True Range values over the past 14 trading days. Traders and analysts may
+    # use this indicator to set stop-loss levels based on the average volatility 
+    # of the security.
+    #
+    # @param high_prices [Array] An array of high prices.
+    # @param low_prices [Array] An array of low prices.
+    # @param close_prices [Array] An array of closing prices.
+    # @param period [Integer] The period for calculating the ATR.
+    # @return [Array] An array of Average True Range values.
+    #
+    def average_true_range(high_prices, low_prices, close_prices, period)
+      true_ranges = calculate_true_range(high_prices, low_prices, close_prices)
+      atr_values = []
+
+      true_ranges.each_with_index do |true_range, index|
+        if index < period - 1
+          atr_values << nil
+        elsif index == period - 1
+          atr_values << true_ranges[0..index].sum / period.to_f
+        else
+          atr_values << (atr_values[index - 1] * (period - 1) + true_range) / period.to_f
+        end
+      end
+
+      atr_values
+    end
+    alias_method :atr, :average_true_range
+
+
   end
 end
 
